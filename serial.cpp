9fa6701a40a284105213e3d9e34039990ddf3cdc
Jerome <zjrtomjohn@hotmail.com>
2020-02-15 16:06:38 -0800

    Bo's change

--- serial.cpp
+++ serial.cpp
@@ -1,5 +1,6 @@
 #include "common.h"
 #include <cmath>
+#include <vector>
 
 // Apply the force from neighbor to particle
 void apply_force(particle_t& particle, particle_t& neighbor) {
@@ -25,10 +26,10 @@ void apply_force(particle_t& particle, particle_t& neighbor) {
 void move(particle_t& p, double size) {
     // Slightly simplified Velocity Verlet integration
     // Conserves energy better than explicit Euler method
-    p.vx += p.ax * dt;
-    p.vy += p.ay * dt;
-    p.x += p.vx * dt;
-    p.y += p.vy * dt;
+    p.vx += p.ax * dtt;
+    p.vy += p.ay * dtt;
+    p.x += p.vx * dtt;
+    p.y += p.vy * dtt;
 
     // Bounce from walls
     while (p.x < 0 || p.x > size) {
@@ -50,6 +51,50 @@ void init_simulation(particle_t* parts, int num_parts, double size) {
 }
 
 void simulate_one_step(particle_t* parts, int num_parts, double size) {
+
+    //Build Bins
+    int bincnt = 200;
+    double binsize = double(size / bincnt);
+    std::vector<particle_t> bins[bincnt][bincnt];
+
+    for (int i = 0; i < num_parts; ++i) {
+        int j = int(parts[i].x / binsize);
+        int k = int(parts[i].y / binsize);
+        bins[j][k].push_back(parts[i]);
+    }
+
+    // Add neighbor bins loops:
+    for (int a = 0; a < bincnt; ++a) {
+        for (int b = 0; b < bincnt; ++b) {
+            for (int i = 0; i < bins[a][b].size(); ++i) {
+                bins[a][b][i].ax = bins[a][b][i].ay = 0;
+                int dirs[9][2] = {{-1, -1},
+                                  {-1, 0},
+                                  {-1, 1},
+                                  {0,  -1},
+                                  {0,  0},
+                                  {0,  1},
+                                  {1,  -1},
+                                  {1,  0},
+                                  {1,  1}};
+                for (int d = 0; d < 9; d++) {
+                    int row = a + dirs[d][0];
+                    int col = b + dirs[d][1];
+                    if (row >= 0 && row < bincnt && col >= 0 && col < bincnt) {
+                        for (int j = 0; j < bins[row][col].size(); ++j) {
+                            apply_force(bins[a][b][i], bins[a][b][j]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    // Move Particles
+    for (int i = 0; i < num_parts; ++i) {
+        move(parts[i], size);
+    }
+    /**
+
     // Compute Forces
     for (int i = 0; i < num_parts; ++i) {
         parts[i].ax = parts[i].ay = 0;
@@ -62,4 +107,6 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
     for (int i = 0; i < num_parts; ++i) {
         move(parts[i], size);
     }
+     **/
 }
+
