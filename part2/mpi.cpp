dbce3785306c2989b57475c5b814563515f40f5f
Jerome <zjrtomjohn@hotmail.com>
2020-03-02 01:32:00 -0800

    Finish initialization

--- part2/mpi.cpp
+++ part2/mpi.cpp
@@ -1,20 +1,81 @@
 #include "common.h"
 #include <mpi.h>
+#include <iostream>
+#include <cmath>
+#include <unordered_set>
 
-// Put any static global variables here that you will use throughout the simulation.
+using namespace std;
 
-void init_simulation(particle_t* parts, int num_parts, double size, int rank, int num_procs) {
-	// You can use this space to initialize data objects that you may need
-	// This function will be called once before the algorithm begins
-	// Do not do any particle simulation here
+///////////////////////////////////// Static Global Variables /////////////////////////////////////
+
+// Length of a bin's side
+#define BIN_SIZE 0.01
+
+int Num_Proc_Per_Side;  // Number of processors per side
+double Proc_Size;  // Size of a processor's side
+int Num_Bins_Per_Proc_Side;  // Number of bins on a processor's side
+
+int My_Row_Idx;  // Row index of this processor
+int My_Col_Idx;  // Column index of this processor
+double My_Min_X;  // Minimum X value of this processor
+double My_Min_Y;  // Minimum Y value of this processor
+
+unordered_set<particle_t*> *Bins;  // The bins belonging to this processor
+
+//////////////////////////////////////// Helper Functions ////////////////////////////////////////
+
+// Calculate the rank that the input particle belongs to
+inline int calculate_particle_rank(const particle_t& pt) {
+    int row_idx = floor(pt.x / Proc_Size);
+    int col_idx = floor(pt.y / Proc_Size);
+    // Processors are assigned in row-major
+    return row_idx * Num_Proc_Per_Side + col_idx;
 }
 
-void simulate_one_step(particle_t* parts, int num_parts, double size, int rank, int num_procs) {
-    // Write this function
+// Insert the input particle to the correct bin
+inline void put_particle_to_bin(particle_t& pt) {
+    int row_idx = floor( (pt.x - My_Min_X) / BIN_SIZE );
+    int col_idx = floor( (pt.y - My_Min_Y) / BIN_SIZE );
+    int idx = row_idx * Num_Bins_Per_Proc_Side + col_idx;
+    Bins[idx].insert(&pt);
 }
 
-void gather_for_save(particle_t* parts, int num_parts, double size, int rank, int num_procs) {
-    // Write this function such that at the end of it, the master (rank == 0)
-    // processor has an in-order view of all particles. That is, the array
-    // parts is complete and sorted by particle id.
+///////////////////////////////////////// Key Functions /////////////////////////////////////////
+
+// Initialize data objects that we need
+void init_simulation(particle_t* parts, int num_parts, double size, int rank, int num_proc) {
+    // Calculate necessary global constants
+    Num_Proc_Per_Side = floor( sqrt(num_proc) );
+    Proc_Size = size / Num_Proc_Per_Side;
+    Num_Bins_Per_Proc_Side = ceil(Proc_Size / BIN_SIZE);
+
+    // Calculate variables specific to this processor
+    My_Row_Idx = rank / Num_Proc_Per_Side;
+    My_Col_Idx = rank % Num_Proc_Per_Side;
+    My_Min_X = My_Row_Idx * Proc_Size;
+    My_Min_Y = My_Col_Idx * Proc_Size;
+
+    // Initialize bins specific to this processor
+    Bins = new unordered_set<particle_t*>[Num_Bins_Per_Proc_Side * Num_Bins_Per_Proc_Side];
+    // Assign particles belonging to this processor to their corresponding bins
+    for (int i = 0; i < num_parts; i++) {
+        particle_t& pt = parts[i];
+        // If particle belongs to this processor
+        if (calculate_particle_rank(pt) == rank) {
+            put_particle_to_bin(pt);
+        }
+    }
+}
+
+void simulate_one_step(particle_t* parts, int num_parts, double size, int rank, int num_proc) {
+    // TODO: implement
+//    cout << "simulate_one_step() at " << rank << "/" << num_proc << endl;
+}
+
+/* Write this function such that at the end of it, the master (rank == 0)
+ * processor has an in-order view of all particles. That is, the array
+ * parts is complete and sorted by particle id. */
+void gather_for_save(particle_t* parts, int num_parts, double size, int rank, int num_proc) {
+    // TODO: implement
+//    cout << "gather_for_save() at " << rank << "/" << num_proc << endl;
 }
